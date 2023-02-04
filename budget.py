69b18b8665fa19025e694cc2a00fa7a88ebd4b96
Nash Baek <nash4comp@gmail.com>
2023-02-04 14:40:28 -0800

    Add getter and setter
    - total budget
    - warning budget limit

--- budget.py
+++ budget.py
@@ -2,5 +2,19 @@ class Budget:
     """
     This class manages budget of user.
     """
-    def __init__(self, total_budget):
+
+    def __init__(self, total_budget, budget_limit):
         self._total_budget = total_budget
+        self._warning_budget_limit = budget_limit
+
+    def get_total_budget(self):
+        return self._total_budget
+
+    def set_warning_budget_limit(self):
+        return self._warning_budget_limit
+
+    def set_total_budget(self, budget):
+        self._total_budget = budget
+
+    def set_warning_budget_limit(self, budget_limit):
+        self._warning_budget_limit = budget_limit
