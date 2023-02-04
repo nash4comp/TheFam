d428c82cca2fde0a85f1bb4fdf5731fe64ba8ad3
Nash Baek <nash4comp@gmail.com>
2023-02-04 13:35:52 -0800

    Add skeleton of user related method

--- user.py
+++ user.py
@@ -2,6 +2,7 @@ class User:
     """
     This class manages users' information.
     """
+
     def __init__(self, user_name, budget):
         self._user_name = user_name
         self._budget = budget
@@ -13,9 +14,49 @@ class User:
         """
         return self._user_name
 
-    def get_budget(self):
+    def get_user_age(self):
+        pass
+
+    def get_user_type(self):
+        pass
+
+    def get_user_bank_account_number(self):
+        pass
+
+    def get_user_bank_balance(self):
+        pass
+
+    def get_user_budget(self):
         """
         Returns budget
         :return: user's budget
         """
         return self._budget
+
+    def add_new_user(self):
+        pass
+
+    def delete_user(self):
+        pass
+
+    def set_user_name(self):
+        """
+        Returns user name
+        :return: user name
+        """
+        return self._user_name
+
+    def set_user_age(self):
+        pass
+
+    def set_user_type(self):
+        pass
+
+    def set_user_bank_account_number(self):
+        pass
+
+    def set_user_bank_balance(self):
+        pass
+
+    def set_user_budget(self):
+        pass
