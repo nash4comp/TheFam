ddfb13fa813820b38ae94ca73f797a9a63cc1e54
InkyungBaek <nash4comp@gmail.com>
2023-02-04 11:16:30 -0800

    Put the student's information and UML url

--- driver.py
+++ driver.py
@@ -4,8 +4,10 @@ Explanation
 # Name1: Nash Baek (nash4comp@gmail.com)
 # Student number1: A01243888
 
-# Name2:
-# Student number1:
+# Name2: Taylor Ji TODO: add email in here
+# Student number1: A01304056
+
+UML diagram: https://app.diagrams.net/#G1DUxHF4SH4QveN8GbqGaHewuu07bC5lDG
 """
 
 
